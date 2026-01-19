1672cf07e7a4c5242b081d7bedaea33d2cc8cb99
Luis Daniel <luisdaniel.martinezprado@gmail.com>
2026-01-18 20:56:00 -0600

    Second Commit

--- app/src/main/java/com/example/kazero/activities/LoginActivity.java
+++ app/src/main/java/com/example/kazero/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("LOGIN", "LoginActivity creada");
         setContentView(R.layout.activity_login);
 
         initViews();
